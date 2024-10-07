16ca1dd780e8e32b2bd0933aa86ef72176f03150
AiryyCodes <airyycodes@gmail.com>
2024-10-07 10:22:43 +0200

    Added more vector types

--- include/CloudEngine/vec.h
+++ include/CloudEngine/vec.h
@@ -1,8 +1,12 @@
 #pragma once
 
-#include <glm/ext/matrix_float4x4.hpp>
+#include <glm/fwd.hpp>
 #include <glm/glm.hpp>
 
 typedef glm::fvec3 fvec3;
+typedef glm::ivec3 ivec3;
+
+typedef glm::fvec2 fvec2;
+typedef glm::ivec2 ivec2;
 
 typedef glm::mat4 mat4;
