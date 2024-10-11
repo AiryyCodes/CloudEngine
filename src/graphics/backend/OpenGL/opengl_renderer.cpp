65c81f0dc9c0905044392735442a37d557d54bd1
AiryyCodes <airyycodes@gmail.com>
2024-10-11 16:56:11 +0200

    Added stb_image_write.h

--- src/graphics/backend/OpenGL/opengl_renderer.cpp
+++ src/graphics/backend/OpenGL/opengl_renderer.cpp
@@ -7,6 +7,8 @@
 
 #include <glad/gl.h>
 #include <glm/ext/matrix_transform.hpp>
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb/stb_image_write.h>
 
 static Mesh mesh;
 static Shader shader(MAIN_VERT, MAIN_FRAG);
@@ -20,7 +22,7 @@ void Renderer::Init()
 
 void Renderer::Update()
 {
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClearColor(0.5f, 0.8f, 1.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
 
